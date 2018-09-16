28351bf2b071f641e00897b7a30eec502e6850b0
Julianne Crawford <julianne.marik@gmail.com>
2018-09-16 11:55:02 -0500

    closest value in BST completed

--- algoExpertProblems.js
+++ algoExpertProblems.js
@@ -1,4 +1,6 @@
 /*
+QUESTION 1: TWO NUMBER SUM - LEVEL: EASY
+------------------------------------------
 Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the target sum, the funtion should return them in an array, in sorted order. If not, the funtion should rturn an empty array. Assume that there will be at most one pair of numbers summing to the target sum.
 */
 // SOLUTION #1 - least optimal
@@ -42,7 +44,40 @@ function twoNumberSum(array, targetSum) {
   return [];
 }
 
+/*
+QUESTION 2: FIND CLOSEST VALUE IN BST - LEVEL: EASY
+----------------------------------------------------
+Given a BST data structure consisiting of BST nodes and a target integer value, write a function that finds the closest value to that target value contained in the BST
+*/
 
-console.log(twoNumberSum([2, 0, -5, 25, 9, 10, -2, 15], 11))
+// SOLUTION 1 - iterative solution:
+// average case: O(log(n) time || O(1) space
+// worst case: O(n) time || O(1) space
+function findClosestValueInBst(tree, target) {
+  let closestValue = tree.value;
+  if (closestValue === target) return closestValue;
+  let direction = (target < tree.value) ? 'left' : 'right';
 
 
+  while(tree[direction]){
+    tree = tree[direction];
+    closestValue = Math.abs(closestValue - target) < Math.abs(tree.value - target) ? closestValue : tree.value;
+    if (closestValue === target) return closestValue;
+    direction = (target < tree.value) ? 'left' : 'right';
+  }
+  return closestValue;
+}
+
+// SOLUTION 2 - recursive solution:
+// average case: O(log(n)) time || O(log(n)) space
+// worst case: O(n) time || O(n) space
+let closestValue = Infinity;
+function findClosestValueInBst(tree, target){
+  closestValue = Math.abs(target - tree.value) < Math.abs(target - closestValue) ? tree.value : closestValue;
+  let direction = (target < tree.value) ? 'left' : 'right';
+
+  if (!tree[direction]){
+    return closestValue;
+  }
+  return findClosestValueInBst(tree[direction], target);
+}
